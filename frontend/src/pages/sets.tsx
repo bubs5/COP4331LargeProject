04381ce4b292c39620f79614be6abd64ad4ee9bb
Ludiferr <sh342741@ucf.edu>
2026-04-03 15:37:38 -0400

    Update sets.tsx

--- frontend/src/pages/sets.tsx
+++ frontend/src/pages/sets.tsx
@@ -1,13 +1,25 @@
 //test import code before full setup
 import { studySets } from '../data/testData';
+import { useNavigate } from "react-router-dom";
 
 function StudySets() {
+
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1>Study Sets</h1>
 
             {studySets.map((set) => (
-                <div key={set.id} className="card">
+                <div
+                    key={set.id}
+                    className="card"
+                    onClick={() => {
+                        localStorage.setItem("lastSet", JSON.stringify(set));
+                        navigate("/flashcards");
+                    }}
+                    style={{ cursor: "pointer" }}
+                >
                     <h3>{set.title}</h3>
                     <p>{set.description}</p>
                     <p>{set.cardCount} cards</p>
